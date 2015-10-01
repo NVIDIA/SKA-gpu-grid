e05cdb72dfde26b7139872b8e407c7a1a7a8b46e
LeviBarnes <levi.barnes@gmail.com>
2015-10-01 16:48:09 -0700

    Need to define PTS
    Added definition for PTS (number of img points
    per thread) in Defines.h. PTS=2 seems to give
    best performance.

--- Defines.h
+++ Defines.h
@@ -1,13 +1,14 @@
 #ifndef __DEFINES_H
 #define __DEFINES_H
 
-#define NPOINTS 40000
-#define GCF_DIM 128 
+#define NPOINTS 400000
+#define GCF_DIM 128
 #define IMG_SIZE 4096
 #define GCF_GRID 8
 //BLOCK_Y affects only MOVING_WINDOW
 #define BLOCK_Y 4 
-
+#define PTS 2
+//#define DEBUG1
 #endif
 
 
